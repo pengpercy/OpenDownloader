6cc561979fdc4d4b2c10f5f8f0bd410317015a43
Percy <pengpercy@users.noreply.github.com>
2026-02-01 08:20:42 +0800

    Fix macOS icon packaging and bump version to 1.0.11

--- build/scripts/package.osx-app.sh
+++ build/scripts/package.osx-app.sh
@@ -21,7 +21,22 @@ mkdir -p OpenDownloader.app/Contents/MacOS
 mkdir -p OpenDownloader.app/Contents/Resources
 cp -r OpenDownloader/* OpenDownloader.app/Contents/MacOS/
 rm -rf OpenDownloader
-cp resources/app/App.icns OpenDownloader.app/Contents/Resources/App.icns
+ICON_SOURCE="resources/_common/icons/opendownloader.png"
+ICONSET_DIR="App.iconset"
+rm -rf "$ICONSET_DIR"
+mkdir -p "$ICONSET_DIR"
+sips -z 16 16     "$ICON_SOURCE" --out "$ICONSET_DIR/icon_16x16.png" >/dev/null
+sips -z 32 32     "$ICON_SOURCE" --out "$ICONSET_DIR/icon_16x16@2x.png" >/dev/null
+sips -z 32 32     "$ICON_SOURCE" --out "$ICONSET_DIR/icon_32x32.png" >/dev/null
+sips -z 64 64     "$ICON_SOURCE" --out "$ICONSET_DIR/icon_32x32@2x.png" >/dev/null
+sips -z 128 128   "$ICON_SOURCE" --out "$ICONSET_DIR/icon_128x128.png" >/dev/null
+sips -z 256 256   "$ICON_SOURCE" --out "$ICONSET_DIR/icon_128x128@2x.png" >/dev/null
+sips -z 256 256   "$ICON_SOURCE" --out "$ICONSET_DIR/icon_256x256.png" >/dev/null
+sips -z 512 512   "$ICON_SOURCE" --out "$ICONSET_DIR/icon_256x256@2x.png" >/dev/null
+sips -z 512 512   "$ICON_SOURCE" --out "$ICONSET_DIR/icon_512x512.png" >/dev/null
+sips -z 1024 1024 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_512x512@2x.png" >/dev/null
+iconutil -c icns "$ICONSET_DIR" -o OpenDownloader.app/Contents/Resources/App.icns
+rm -rf "$ICONSET_DIR"
 sed "s/OPENDOWNLOADER_VERSION/$VERSION/g" resources/app/App.plist > OpenDownloader.app/Contents/Info.plist
 rm -rf OpenDownloader.app/Contents/MacOS/OpenDownloader.dsym
 
